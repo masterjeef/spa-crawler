ca7f377a4de04161b5465d257de39b64ae7e268b
Jeff Linton <jslinton44@gmail.com>
2018-05-25 10:49:56 -0500

    Project setup

---
+++ src/SpaCrawler.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿
+namespace SpaCrawler.Console
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("Press enter to stop.");
+            System.Console.Read();
+        }
+    }
+}
